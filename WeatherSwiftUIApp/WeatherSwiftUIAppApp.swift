8c906d01c905de187b32d7499b01725d24419782
Aidin <aidin.abdurakhmanov@gmail.com>
2021-04-23 22:35:17 +0300

    Initial Commit

---
+++ WeatherSwiftUIApp/WeatherSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIAppApp.swift
+//  WeatherSwiftUIApp
+//
+//  Created by Айдин Абдурахманов on 23.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
